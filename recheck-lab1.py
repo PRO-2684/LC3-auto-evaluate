c222f7ec4163c70ac27bd50f3a86d7bbe731c671
PRO-2684 <54608551+PRO-2684@users.noreply.github.com>
2023-11-12 21:40:44 +0800

    Update re-check

--- recheck-lab1.py
+++ recheck-lab1.py
@@ -1,4 +1,4 @@
-from json import load
+from json import load, dump
 from re import compile, search
 
 with open("./student/lab1.json") as f:
@@ -18,6 +18,15 @@ for fname, out in data.items():
     m = search(PTRN_MEM, out)
     mem = int(m.group(1)) if m else -1
     if mem == -1:
-        print(f"Student {stuId} gave no answer")
+        print(f"Student {stuId} failed to compile or timeout")
+        assert data[fname]["score"] == 0
+        data[fname]["log"] += "\nRe-check: Failed to compile or timeout"
     elif not stuEnd == mem:
         print(f'Student {stuId} gave wrong answer "{mem}" (expected "{stuEnd}")')
+        data[fname]["score"] = data[fname]["score"] // 2 # half score if not match
+        data[fname]["log"] += f"\nRe-check: Mem at 0x3101 ({mem}) does not match last digit of student ID ({stuEnd}), half score"
+    else:
+        data[fname]["log"] += f"\nRe-check: Correct"
+
+with open("./student/lab1.json", "w") as f:
+    dump(data, f, indent=4, ensure_ascii=False)
